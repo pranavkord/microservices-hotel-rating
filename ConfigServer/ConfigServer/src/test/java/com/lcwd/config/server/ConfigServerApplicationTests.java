f259701ee3121e835359f6aca8966cb9f3d69286
Pranav Korde <pranavkorde01@gmail.com>
2023-05-15 09:41:53 +0530

    test cases uncommenting

--- ConfigServer/ConfigServer/src/test/java/com/lcwd/config/server/ConfigServerApplicationTests.java
+++ ConfigServer/ConfigServer/src/test/java/com/lcwd/config/server/ConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.lcwd.config.server;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ConfigServerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.lcwd.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- UserService/UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
+++ UserService/UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
@@ -1,30 +1,30 @@
-//package com.lcwd.user.service;
-//
-//import com.psldemo.user.service.entities.Rating;
-//import com.psldemo.user.service.external.services.RatingService;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.ResponseEntity;
-//
-//@SpringBootTest
-//class UserServiceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
+package com.lcwd.user.service;
+
+import com.psldemo.user.service.entities.Rating;
+import com.psldemo.user.service.external.services.RatingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Autowired
+    private RatingService ratingService;
+
+  //  @Test
+//    void createRating() {
+//        Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("this is created using feign client").build();
+////        ResponseEntity<Rating> ratingResponseEntity = ratingService.createRating(rating);
+//        System.out.println("new rating created");
 //    }
-//
-//
-//    @Autowired
-//    private RatingService ratingService;
-//
-//  //  @Test
-////    void createRating() {
-////        Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("this is created using feign client").build();
-//////        ResponseEntity<Rating> ratingResponseEntity = ratingService.createRating(rating);
-////        System.out.println("new rating created");
-////    }
-//
-//
-//}
+
+
+}
